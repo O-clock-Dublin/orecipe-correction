798ed958517c5a7fd18a0a22313c074108ecfee7
QuentinCaffray <quentin.caffray@oclock.school>
2025-12-17 17:40:56 +0100

    Ajout du formulaire de connexion dans le composant Header et suppression du contexte utilisateur

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,12 +1,42 @@
 import styles from "./header.module.css";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [token, setToken] = useState<string | null>(null);
+  const [pseudo, setPseudo] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("emailForm") as string;
+    const password = formData.get("passwordForm") as string;
+
+    // On va faire la requête API ici (prochaine étape)
+    console.log("Email:", email, "Password:", password);
+  }
+
   return (
     <header className={styles.header}>
       <a href="/">
         <img src="/vite.svg" alt="o'clock recipes" />
       </a>
-      <p>O'Recipes</p>
+      {pseudo ? (
+        <p>Bienvenue, {pseudo} !</p>
+      ) : (
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <input type="email" name="emailForm" placeholder="Adresse email" />
+          <input
+            type="password"
+            name="passwordForm"
+            placeholder="Mot de passe"
+          />
+          <button type="submit" name="submitButton">
+            Se connecter
+          </button>
+        </form>
+      )}
     </header>
   );
 }
