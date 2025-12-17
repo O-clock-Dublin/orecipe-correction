df9a8bcf515c879c8185d5d93aff30ec09b895f9
Nowak Perry-Këanou <nwkperry-k@mac.home>
2025-12-17 16:02:34 +0100

    feat: amélioration des userContext + création du UserProvider et ajout du composant dans main.tsx

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,20 +1,49 @@
-import { createContext } from "react"
+import { createContext, useState, ReactNode } from "react"
 
 // Je prépare les boites que je veux mettre dans mon carton
 // Je type les différentes valeurs de chaque boite
 // Ces boites sont pour le moment vides
 interface UserContextType {
-  token: null | string
-  username: null | string
+  token: string | null
+  username: string | null
+  error: string | null
   setToken: (token: string | null) => void
   setUsername: (username: string | null) => void
+  setError: (error: string | null) => void
 }
 
 //Je crée le carton avec deux boites vides dedans token et username
 const UserContext = createContext<UserContextType>({
   token: null,
   username: null,
+  error: null,
   setToken: () => {},
   setUsername: () => {},
+  setError: () => {},
 })
+
+
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState<string | null>(null)
+  const [username, setUsername] = useState<string | null>(null)
+  const [error, setError] = useState<string | null>(null)
+
+  return (
+    <UserContext.Provider
+      value={{
+        token,
+        username,
+        error,
+        setToken,
+        setUsername,
+        setError,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+
 export default UserContext
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { BrowserRouter } from "react-router"
+import { UserProvider } from "./context/userContext"
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>
 )
